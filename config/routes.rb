888b4e1b0106a543a02d0c79bc94e114ad15ff82
Sanjeevani Choudhery <schoudhery@tamu.edu>
2019-10-20 17:31:59 +0000

    launch

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
-  resources :movies
+  #resources :movies
   # map '/' to be a redirect to '/movies'
-  root :to => redirect('/movies')
+  #root :to => redirect('/movies')
+  root 'movies#index'
+  resources :movies
 end
